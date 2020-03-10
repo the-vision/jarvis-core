86cecef1aa18272fa9b02f4655bd10d5944fa8ea
hritikpapa <kvnl.hritik@gmail.com>
2020-03-10 20:28:59 +0530

    added joke intent

--- test.py
+++ test.py
@@ -75,6 +75,11 @@ if '__main__' == __name__:
             'intent': 'help',
             'entities': []
         },
+        {
+            'input': 'random joke',
+            'intent': 'joke',
+            'entities': []
+        },
     ]
 
     for query in queries:
